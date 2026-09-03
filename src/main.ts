2e935101efadf1cfef009955e4576e661ba00715
hoducvinhh <hoducvinh2k4@gmail.com>
2026-09-03 16:04:39 +0700

    feat: initialize NestJS application with basic structure and configurations
    - Created app.module.ts to define the main application module.
    - Implemented main.ts to bootstrap the application with CORS and cookie parser.
    - Added utility function parseEnvOrigins to handle environment variable origins.
    - Set up end-to-end testing with app.e2e-spec.ts for basic GET request.
    - Configured Jest for end-to-end testing in jest-e2e.json.
    - Added TypeScript configuration for building in tsconfig.build.json.
    - Established base TypeScript configuration in tsconfig.json.

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { parseEnvOrigins } from './utils/parse-env-origins';
+
+
+const getCorsAllowList = () => {
+
+  return parseEnvOrigins(process.env.CLIENT_URL, process.env.CORS_OTHER_URL);
+}
+
+
+async function bootstrap() {
+
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
+  const alowList = getCorsAllowList();
+
+  app.enableCors({
+    origin: (requestOrigin: string, callback) => {
+      if (!requestOrigin) {
+        callback(null, true);
+        return;
+      }
+
+      if (alowList.includes(requestOrigin)) {
+        callback(null, true);
+        return;
+      }
+
+
+      // log warning
+
+      callback(null, false);
+    },
+
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+
+    credentials: true,
+  });
+
+
+  await app.listen(process.env.PORT ?? 8080);
+}
+bootstrap();
